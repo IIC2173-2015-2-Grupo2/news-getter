8da111160965394234915fe3500e5b7f12e6dbc4
fisimon <fisimon@uc.cl>
2015-09-27 17:51:39 -0300

    theguardian ahora funcionando

--- NewsGetter/scheduler.rb
+++ NewsGetter/scheduler.rb
@@ -5,9 +5,12 @@ class Scheduler
 
   def start
     worker1 = Worker.new Emol.new
+    worker2 = Worker.new TheGuardian.new
     while true
 
       worker1.work
+      sleep(3)
+      worker2.work
       sleep(3)
     end
 
