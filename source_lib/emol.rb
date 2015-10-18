5c2e64eba45885d00f42a8ad6f624a18c3f443c5
iaferrerPUC <iaferrer@uc.cl>
2015-10-18 19:45:35 -0300

    Obtiene tags solo para La Tercera

--- source_lib/emol.rb
+++ source_lib/emol.rb
@@ -2,35 +2,41 @@ require 'nokogiri'
 require 'open-uri'
 require 'date'
 require 'time'
-require './utilities'
+require './source_lib/utilities'
 
 
 class Emol
-    def fetch_news(last_fetch = "#{Date.today.to_s} 00:00:00")
-      news = Array.new
-      year  = Date.today.strftime("%Y")
-      month = Date.today.strftime("%m")
-      day = Date.today.strftime("%d")
+  def fetch_news(last_fetch = "#{Date.today.to_s} 00:00:00")
+    news = Array.new
+    year  = Date.today.strftime("%Y")
+    month = Date.today.strftime("%m")
+    day = Date.today.strftime("%d")
 
-      doc = Nokogiri::HTML(open("http://www.emol.com/noticias/#{year}/#{month}/#{day}"))
+    doc = Nokogiri::HTML(open("http://www.emol.com/noticias/#{year}/#{month}/#{day}"))
 
-      news = doc.xpath("//li[contains(@id, 'cuTodas_ucCajaTodas_ListadoNoticias')]").collect do |node|
-        time = node.xpath("div[contains(@class, 'hora_listado')]/text()")
-        title = node.xpath("div[contains(@class, 'listado')]/a/text()")
-        header = node.xpath("div[contains(@class, 'listado')]/span/text()")
-        url = node.xpath("div[contains(@class, 'listado')]/a/@href")
+    news = doc.xpath("//li[contains(@id, 'cuTodas_ucCajaTodas_ListadoNoticias')]").collect do |node|
+      time = node.xpath("div[contains(@class, 'hora_listado')]/text()")
+      title = node.xpath("div[contains(@class, 'listado')]/a/text()")
+      header = node.xpath("div[contains(@class, 'listado')]/span/text()")
+      url = node.xpath("div[contains(@class, 'listado')]/a/@href")
+      tags = ""
 
-        doc2 = Nokogiri::HTML(open(url.to_s))
-        body = doc2.xpath("//div[contains(@id, 'cuDetalle_cuTexto_textoNoticia')]").first
-        
-        time = to_time(year, month, day, time.to_s)
-        break if !earlier?(last_fetch, time)
-        body = parseBody body.to_s
+      time = to_time(year, month, day, time.to_s)
+      break if !earlier?(last_fetch, time)
 
-        noticia = {title: "#{title}", time: "#{time}", header: "#{header}", url: "#{url}" , body: "#{body}"}
+      doc2 = Nokogiri::HTML(open(url.to_s))
+      body = fetch_body doc2
 
-        noticia
-      end
-      news
+      noticia = {title: "#{title}", time: "#{time}", header: "#{header}", url: "#{url}", body: "#{body}", tags: "#{tags}" }
+
+      noticia
     end
+    news
+  end
+
+  def fetch_body(body)
+    aux = body.xpath("//div[contains(@id, 'cuDetalle_cuTexto_textoNoticia')]").first
+    aux = parseBody aux.to_s
+    return aux
+  end
 end
--- source_lib/latercera.rb
+++ source_lib/latercera.rb
@@ -2,36 +2,52 @@ require 'nokogiri'
 require 'open-uri'
 require 'date'
 require 'time'
-require './utilities'
+require './source_lib/utilities'
 
 
 class LaTercera
-    def fetch_news(last_fetch = "#{Date.today.to_s} 00:00:00")
-      news = Array.new
-      year  = Date.today.strftime("%Y")
-      month = Date.today.strftime("%m")
-      day = Date.today.strftime("%d")
+  def fetch_news(last_fetch = "#{Date.today.to_s} 00:00:00")
+    news = Array.new
+    
+    year  = Date.today.strftime("%Y")
+    month = Date.today.strftime("%m")
+    day = Date.today.strftime("%d")
 
-      doc = Nokogiri::HTML(open("http://www.tercera.cl/feed/manager?type=rss&sc=TEFURVJDRVJB"))
+    doc = Nokogiri::HTML(open("http://www.tercera.cl/feed/manager?type=rss&sc=TEFURVJDRVJB"))
 
-      news = doc.xpath("//item").collect do |node|
-        time = node.xpath("pubdate/text()")
-        title = node.xpath("title/text()")
-        header = node.xpath("description/text()")
-        url = node.xpath("guid/text()")
+    news = doc.xpath("//item").collect do |node|
+      time = node.xpath("pubdate/text()")
+      title = node.xpath("title/text()")
+      header = node.xpath("description/text()").text
+      url = node.xpath("guid/text()")
 
-        doc2 = Nokogiri::HTML(open(url.to_s))
-        body = doc2.xpath("//div[contains(@class, 'article-center-text')]").first
-        
-        time = parseTime time.to_s
-        break if !earlier?(last_fetch, time)
-        header = parseHeader header.to_s
-        body = parseBody body.to_s
+      time = parseTime time.to_s
+      break if !earlier?(last_fetch, time)
 
-        noticia = {title: "#{title}", time: "#{time}", header: "#{header}", url: "#{url}", body: "#{body}" }
+      doc2 = Nokogiri::HTML(open(url.to_s))
+      body = fetch_body doc2
+      tags = fetch_tags doc2
 
-        noticia
-      end
-      news
+      noticia = {title: "#{title}", time: "#{time}", header: "#{header}", url: "#{url}", body: "#{body}", tags: "#{tags}" }
+
+      noticia
     end
+    news
+  end
+
+  def fetch_body(body)
+    aux = ""
+    body.xpath("//div[contains(@class, 'article-center-text')]/p/text()").each do |b|
+      aux = aux + b.text + "\n"
+    end
+    return aux
+  end
+
+  def fetch_tags(body)
+    aux = []
+    body.xpath("//ul[contains(@class, 'listags')]/li/a/text()").each do |f|
+      aux << f.text
+    end
+    return aux
+  end
 end
