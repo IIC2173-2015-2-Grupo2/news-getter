a1996b69dba601bb44593947987fe4ec7dce3c6e
Felipe <fisimon@uc.cl>
2015-11-27 00:40:15 -0300

    ready to send news

--- test.rb
+++ test.rb
@@ -1,3 +1,8 @@
 require './loader'
 
-Loader.load("./source_lib/emol.json")
+adapter = Adapter.new
+adapter.clear_pages
+while true
+  Loader.load_pages("http://arquiapi.ing.puc.cl/news")
+  sleep(30)
+end
